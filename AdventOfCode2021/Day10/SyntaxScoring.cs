80222ecfbc78ca8488fe1c2d273cdd8e3bb9fda5
Afek96 <adam.malcherczyk96@gmail.com>
2022-01-06 12:24:17 +0100

    Day 10 Part one - complete.

--- AdventOfCode2021/Day10/SyntaxScoring.cs
+++ AdventOfCode2021/Day10/SyntaxScoring.cs
@@ -11,7 +11,29 @@ namespace AdventOfCode2021.Day10
     {
         internal override string GetFirstResult(string inputText)
         {
-            return "";
+            Dictionary<char, int> corrupted = new Dictionary<char, int>() { { ')', 0 }, { '>', 0 }, { ']', 0 }, { '}', 0 } };
+            StringBuilder sb = new StringBuilder();
+            foreach (var line in inputText.Split("\r\n"))
+            {
+                var stack = new Stack<char>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] is '(' or '<' or '[' or '{') { 
+                        stack.Push(line[i]);
+                        continue;
+                    }
+
+                    var expected = stack.Pop();
+                    if (expected != Convert.ToChar(line[i] - 1) && expected != Convert.ToChar(line[i] - 2))
+                    {
+                        sb.AppendLine(line);
+                        corrupted[line[i]]++;
+                        break;
+                    }
+                }
+            }
+            var corr = sb.ToString();
+            return (corrupted[')'] * 3 + corrupted[']'] * 57 + corrupted['}'] * 1197 + corrupted['>'] * 25137).ToString();
         }
 
         internal override string GetSecondResult(string inputText)
@@ -20,3 +42,4 @@ namespace AdventOfCode2021.Day10
         }
     }
 }
+
