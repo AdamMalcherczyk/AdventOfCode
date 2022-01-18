3ed2f7d6db5cca10d8f8976f4aab1ef171dde5a7
Afek96 <adam.malcherczyk96@gmail.com>
2022-01-18 18:30:27 +0100

    Prapared Day 12.

---
+++ AdventOfCode2021/Day12/PassagePathing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day12
+{
+    internal class PassagePathing : Base
+    {
+        internal override string GetFirstResult(string inputText)
+        {
+            return "";
+        }
+
+        internal override string GetSecondResult(string inputText)
+        {
+            return "";
+        }
+    }
+}
+
---
+++ AdventOfCode2021Tests/Day12/PassagePathingTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2021.Day12;
+
+namespace AdventOfCode2021Tests.Day12
+{
+    public class PassagePathingTests
+    {
+        private PassagePathing _sut;
+
+        private string _firstTestInput =
+            @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+";
+        private string _secondTestInput =
+            @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+";
+        
+        private string _thirdTestInput =
+            @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+";
+        public PassagePathingTests()
+        {
+            _sut = new PassagePathing();
+        }
+
+        [Fact()]
+        public void GetFirstResult_ForFirstInputTest()
+        {
+            var result = _sut.GetFirstResult(_firstTestInput);
+
+            Assert.Equal("10", result);
+        }
+        
+        public void GetFirstResult_ForSecondInputTest()
+        {
+            var result = _sut.GetFirstResult(_secondTestInput);
+
+            Assert.Equal("19", result);
+        }
+        
+        public void GetFirstResult_ForThirdInputTest()
+        {
+            var result = _sut.GetFirstResult(_thirdTestInput);
+
+            Assert.Equal("226", result);
+        }
+
+        [Fact()]
+        public void GetSecondResultTest()
+        {
+            var result = _sut.GetSecondResult(_firstTestInput);
+
+            Assert.Equal("", result);
+        }
+    }
+}
