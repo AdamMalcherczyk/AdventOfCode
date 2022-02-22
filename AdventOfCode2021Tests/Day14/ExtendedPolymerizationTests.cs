796515e51d1e11d2e82f2f4143f308fa490220d7
Afek96 <adam.malcherczyk96@gmail.com>
2022-02-23 00:25:52 +0100

    Prepared Day14.

---
+++ AdventOfCode2021Tests/Day14/ExtendedPolymerizationTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2021.Day14;
+
+namespace AdventOfCode2021Tests.Day14
+{
+    public class ExtendedPolymerizationTests
+    {
+        private ExtendedPolymerization _sut;
+
+        private string _testInput =
+            @"NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+";
+        public ExtendedPolymerizationTests()
+        {
+            _sut = new ExtendedPolymerization();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("1588", result);
+        }
+
+        [Fact()]
+        public void GetSecondResult_ForFirstInputTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("-1", result);
+        }
+    }
+}
