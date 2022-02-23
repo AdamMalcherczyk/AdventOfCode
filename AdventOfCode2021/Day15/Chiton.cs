0fe591677674252ce6f7c0257b0e97afa576fcf3
Afek96 <adam.malcherczyk96@gmail.com>
2022-02-23 21:35:31 +0100

    Prepared Day 15.

---
+++ AdventOfCode2021/Day15/Chiton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day15
+{
+    internal class Chiton : Base
+    {
+        internal override string GetFirstResult(string inputText)
+        {
+            return "";
+        }
+
+
+        internal override string GetSecondResult(string inputText)
+        {
+            return "";
+        }
+    }
+}
+
---
+++ AdventOfCode2021Tests/Day15/ChitonTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2021.Day15;
+
+namespace AdventOfCode2021Tests.Day15
+{
+    public class ChitonTests
+    {
+        private Chiton _sut;
+
+        private string _testInput =
+            @"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+";
+        public ChitonTests()
+        {
+            _sut = new Chiton();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("40", result);
+        }
+
+        [Fact()]
+        public void GetSecondResult_ForFirstInputTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("-1", result);
+        }
+    }
+}
