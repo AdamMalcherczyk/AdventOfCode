d428b0628b375a51ef493dd6bd78927c5cdcdd00
Afek96 <adam.malcherczyk96@gmail.com>
2022-03-03 00:44:27 +0100

    Prepared Day 17.

---
+++ AdventOfCode2021Tests/Day17/TrickShotTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2021.Day17;
+
+namespace AdventOfCode2021Tests.Day17
+{
+    public class TrickShotTests
+    {
+        private string _testInput =
+            @"target area: x=20..30, y=-10..-5";
+
+        private TrickShot _sut;
+        public TrickShotTests()
+        {
+            _sut = new TrickShot();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("45", result);
+        }
+
+        [Fact()]
+        public void GetSecondResult_ForFirstInputTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("-1", result);
+        }
+    }
+}
