6e0b5fef8f10f0c173326a59ad4512c4a60b15e7
Afek96 <adam.malcherczyk96@gmail.com>
2022-03-02 23:37:11 +0100

    Day 16 Part One and Two - complete.

--- AdventOfCode2021/Day16/PacketDecoder.cs
+++ AdventOfCode2021/Day16/PacketDecoder.cs
@@ -8,16 +8,170 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2021.Day16
 {
+    internal enum PackageType
+    {
+        Sum = 0,
+        Product = 1,
+        Min = 2,
+        Max = 3,
+        Literal = 4,
+        GreaterThan = 5,
+        LessThan = 6,
+        Equal = 7
+    }
+
+    internal class Packet
+    {
+        private long _value;
+
+        public long Version { get; set; }
+        public PackageType TypeId { get; set; }
+        public long Value { get => CalculateValue(); set => _value = value; }
+
+        public List<Packet> SubPackets { get; set; } = new List<Packet>();
+
+        public long TotalVersionSum { get => SubPackets.Sum(x => x.TotalVersionSum) + Version; }
+
+        private long CalculateValue()
+        {
+            switch (TypeId)
+            {
+                case PackageType.Sum:
+                    return SubPackets.Sum(x => x.Value);
+                case PackageType.Product:
+                    return GetProduct();
+                case PackageType.Min:
+                    return SubPackets.Min(x => x.Value);
+                case PackageType.Max:
+                    return SubPackets.Max(x => x.Value);
+                case PackageType.Literal:
+                    return _value;
+                case PackageType.GreaterThan:
+                    return SubPackets[0].Value > SubPackets[1].Value ? 1 : 0;
+                case PackageType.LessThan:
+                    return SubPackets[0].Value < SubPackets[1].Value ? 1 : 0;
+                case PackageType.Equal:
+                    return SubPackets[0].Value == SubPackets[1].Value ? 1 : 0;
+                default:
+                    //will never happend
+                    return 0;
+            }
+        }
+
+        private long GetProduct()
+        {
+            var result = 1L;
+            foreach (var packet in SubPackets)
+            {
+                result *= packet.Value;
+            }
+            return result;
+        }
+    }
+
     internal class PacketDecoder : Base
     {
         internal override string GetFirstResult(string inputText)
         {
-            return "";
+            Queue<bool> bits = new Queue<bool>();
+            var bytes = StringToByteArray(inputText);
+            foreach (var word in bytes)
+            {
+                for (int i = 7; i >= 0; i--)
+                {
+                    bits.Enqueue((word >> i) % 2 == 1);
+                }
+            }
+
+            Packet mainPacket = ParsePacket(bits);
+            return mainPacket.TotalVersionSum.ToString();
+        }
+
+        private Packet ParsePacket(Queue<bool> bits)
+        {
+            var packet = new Packet();
+
+            packet.Version = GetValueOf(3, bits);
+            packet.TypeId = (PackageType)GetValueOf(3, bits);
+
+            if (packet.TypeId == PackageType.Literal)
+            {
+                bool shouldContinue;
+                long value = 0;
+                do
+                {
+                    shouldContinue = GetValueOf(1, bits) == 1;
+                    value = GetValueOf(4, bits, value);
+                }
+                while (shouldContinue);
+                packet.Value = value;
+
+                return packet;
+            }
+            
+            long lengthTypeId = GetValueOf(1, bits);
+            if (lengthTypeId == 0)
+            {
+                long subPacketsLength = GetValueOf(15, bits);
+                long expectedEndBitCount = bits.Count - subPacketsLength;
+                do
+                {
+                    packet.SubPackets.Add(ParsePacket(bits));
+                } while (bits.Count != expectedEndBitCount);
+
+                return packet;
+            }
+
+            long subPacketsAmount = GetValueOf(11, bits);
+            for (int i = 0; i < subPacketsAmount; i++)
+            {
+                packet.SubPackets.Add(ParsePacket(bits));
+            }
+
+            return packet;
+        }
+
+        private long GetValueOf(int numberOfBits, Queue<bool> bits, long initialResult = 0)
+        {
+            long result = initialResult;
+            for (int i = 0; i < numberOfBits; i++)
+            {
+                if (bits.Dequeue())
+                {
+                    result = result << 1;
+                    result += 1;
+                }
+                else
+                {
+                    result = result << 1;
+                }
+                
+            }
+            return result;
         }
 
         internal override string GetSecondResult(string inputText)
         {
-            return "";
+            Queue<bool> bits = new Queue<bool>();
+            var bytes = StringToByteArray(inputText);
+            foreach (var word in bytes)
+            {
+                for (int i = 7; i >= 0; i--)
+                {
+                    bits.Enqueue((word >> i) % 2 == 1);
+                }
+            }
+
+            Packet mainPacket = ParsePacket(bits);
+            return mainPacket.Value.ToString();
+        }
+
+        private byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
         }
     }
 }
--- AdventOfCode2021Tests/Day16/PacketDecoderTests.cs
+++ AdventOfCode2021Tests/Day16/PacketDecoderTests.cs
@@ -11,21 +11,16 @@ namespace AdventOfCode2021Tests.Day16
     public class PacketDecoderTests
     {
         private PacketDecoder _sut;
-
-        private const string _testInput = @"8A004A801A8002F478";
-        private const string _testInput2 = @"620080001611562C8802118E34";
-        private const string _testInput3 = @"C0015000016115A2E0802F182340";
-        private const string _testInput4 = @"A0016C880162017C3686B18A3D4780";
         public PacketDecoderTests()
         {
             _sut = new PacketDecoder();
         }
 
         [Theory()]
-        [InlineData(_testInput, "16")]
-        [InlineData(_testInput2, "12")]
-        [InlineData(_testInput3, "23")]
-        [InlineData(_testInput4, "31")]
+        [InlineData("8A004A801A8002F478", "16")]
+        [InlineData("620080001611562C8802118E34", "12")]
+        [InlineData("C0015000016115A2E0802F182340", "23")]
+        [InlineData("A0016C880162017C3686B18A3D4780", "31")]
         public void GetFirstResultTest(string input, string output)
         {
             var result = _sut.GetFirstResult(input);
@@ -34,10 +29,14 @@ namespace AdventOfCode2021Tests.Day16
         }
 
         [Theory()]
-        [InlineData(_testInput, "-1")]
-        [InlineData(_testInput2, "-1")]
-        [InlineData(_testInput3, "-1")]
-        [InlineData(_testInput4, "-1")]
+        [InlineData("C200B40A82", "3")]
+        [InlineData("04005AC33890", "54")]
+        [InlineData("880086C3E88112", "7")]
+        [InlineData("CE00C43D881120", "9")]
+        [InlineData("D8005AC2A8F0", "1")]
+        [InlineData("F600BC2D8F", "0")]
+        [InlineData("9C005AC2F8F0", "0")]
+        [InlineData("9C0141080250320F1802104A08", "1")]
         public void GetSecondResult_ForFirstInputTest(string input, string output)
         {
             var result = _sut.GetSecondResult(input);
