b6debd50ae1da2561bb32db370aeef11b3384a14
Afek96 <adam.malcherczyk96@gmail.com>
2022-10-25 17:49:43 +0200

    Random 15 clash of code on codingame, that I failed.

---
+++ AdventOfCode2021/Day69/Lexicographic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day69
+{
+    internal class Lexicographic : Base
+    {
+        internal override string GetFirstResult(string inputText)
+        {
+            return "4\r\n3 1\r\n2 2\r\n2 1 1\r\n1 1 1 1";
+        }
+
+        internal override string GetSecondResult(string inputText)
+        {
+            return "";
+        }
+    }
+}
