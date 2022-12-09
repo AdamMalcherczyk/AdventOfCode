6fdafaf221aaad0bd45c742e7a3c5e05bc3e1a14
Afek96 <adam.malcherczyk96@gmail.com>
2022-12-09 18:25:04 +0100

    Day 4

---
+++ AdventOfCode2022/Day04/CampCleanup.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day04
+{
+    internal class CampCleanup : Base
+    {
+        public override string GetFirstResult(string inputText)
+        {
+            var pairs = inputText.Split("\r\n");
+
+            var result = 0;
+            foreach (var pair in pairs)
+            {
+                var duty = pair.Split(',');
+
+                var minFirst = int.Parse(duty[0].Split('-')[0]);
+                var maxFirst = int.Parse(duty[0].Split('-')[1]);
+                var minSecond = int.Parse(duty[1].Split('-')[0]);
+                var maxSecond = int.Parse(duty[1].Split('-')[1]);
+
+                if (minFirst >= minSecond && maxFirst <= maxSecond)
+                    result++;
+
+                else if (minSecond >= minFirst && maxSecond <= maxFirst)
+                    result++;
+            }
+            return result.ToString();
+        }
+
+        public override string GetSecondResult(string inputText)
+        {
+            var pairs = inputText.Split("\r\n");
+
+            var result = 0;
+            foreach (var pair in pairs)
+            {
+                var duty = pair.Split(',');
+
+                var minFirst = int.Parse(duty[0].Split('-')[0]);
+                var maxFirst = int.Parse(duty[0].Split('-')[1]);
+                var minSecond = int.Parse(duty[1].Split('-')[0]);
+                var maxSecond = int.Parse(duty[1].Split('-')[1]);
+
+                if (Math.Max(maxFirst, maxSecond) - Math.Min(minFirst, minSecond) + 1 < (maxFirst - minFirst + 1) + (maxSecond - minSecond + 1))
+                    result++;
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2022Tests/Day04/CampCleanupTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2022.Day04;
+
+namespace AdventOfCode2021Tests.Day04
+{
+    public class CampCleanupTests
+    {
+        CampCleanup _sut;
+
+        string _testInput =
+            @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+        public CampCleanupTests()
+        {
+            _sut = new CampCleanup();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("2", result);
+        }
+
+        [Fact()]
+        public void GetSecondResultTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("4", result);
+        }
+    }                                
+}                                    
