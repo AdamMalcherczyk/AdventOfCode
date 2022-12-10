87892eee411a864dba9b64ebb9a96d6d8af248b0
Afek96 <adam.malcherczyk96@gmail.com>
2022-12-10 13:38:04 +0100

    Day 5

---
+++ AdventOfCode2022/Day05/SupplyStacks.cs
@@ -0,0 +1,106 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day05
+{
+    internal class SupplyStacks : Base
+    {
+        public override string GetFirstResult(string inputText)
+        {
+            var inputs = inputText.Split("\r\n\r\n");
+            var rawStack = inputs[0].Split("\r\n");
+            inputs = inputs[1].Split("\r\n");
+
+            Stack<char>[] stacks = new Stack<char>[(rawStack[0].Length+1)/4];
+            foreach (var stackLine in rawStack.Take(rawStack.Length-1))
+            {
+                for (int i = 0; i < (stackLine.Length+1) / 4; i++)
+                {
+                    if (stackLine[i * 4 + 1] == ' ')
+                        continue;
+
+                    if (stacks[i] == null)
+                        stacks[i] = new Stack<char>();
+
+                    stacks[i].Push(stackLine[i * 4 + 1]);
+                }
+            }
+
+            // reversing stacks
+            for (int i = 0; i < stacks.Length; i++)
+            {
+                stacks[i] = new Stack<char>(stacks[i]);
+            }
+
+            foreach (var input in inputs)
+            {
+                var command = input.Split(new string[] { "move ", " from ", " to " }, StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+                for (int i = 0; i < command[0]; i++)
+                {
+                    stacks[command[2] - 1].Push(stacks[command[1] - 1].Pop());
+                }
+            }
+
+            string result = "";
+            for (int i = 0; i < stacks.Length; i++)
+            {
+                result += stacks[i].Peek();
+            }
+
+            return result;
+        }
+
+        public override string GetSecondResult(string inputText)
+        {
+            var inputs = inputText.Split("\r\n\r\n");
+            var rawStack = inputs[0].Split("\r\n");
+            inputs = inputs[1].Split("\r\n");
+
+            Stack<char>[] stacks = new Stack<char>[(rawStack[0].Length + 1) / 4];
+            foreach (var stackLine in rawStack.Take(rawStack.Length - 1))
+            {
+                for (int i = 0; i < (stackLine.Length + 1) / 4; i++)
+                {
+                    if (stackLine[i * 4 + 1] == ' ')
+                        continue;
+
+                    if (stacks[i] == null)
+                        stacks[i] = new Stack<char>();
+
+                    stacks[i].Push(stackLine[i * 4 + 1]);
+                }
+            }
+
+            // reversing stacks
+            for (int i = 0; i < stacks.Length; i++)
+            {
+                stacks[i] = new Stack<char>(stacks[i]);
+            }
+
+            foreach (var input in inputs)
+            {
+                var command = input.Split(new string[] { "move ", " from ", " to " }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+                char[] popped = new char[command[0]];
+                for (int i = 0; i < command[0]; i++)
+                {
+                    popped[i] = stacks[command[1] - 1].Pop();
+                }
+
+                stacks[command[2] - 1] = new Stack<char>(stacks[command[2] - 1].Reverse().Concat(popped.Reverse()));
+            }
+
+            string result = "";
+            for (int i = 0; i < stacks.Length; i++)
+            {
+                result += stacks[i].Peek();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2022Tests/Day05/SupplyStacksTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2022.Day05;
+
+namespace AdventOfCode2022Tests.Day05
+{
+    public class SupplyStacksTests
+    {
+        SupplyStacks _sut;
+
+        string _testInput =
+            @"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+        public SupplyStacksTests()
+        {
+            _sut = new SupplyStacks();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("CMZ", result);
+        }
+
+        [Fact()]
+        public void GetSecondResultTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("MCD", result);
+        }
+    }
+}
