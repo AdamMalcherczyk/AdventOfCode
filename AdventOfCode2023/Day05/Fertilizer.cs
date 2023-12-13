1daef5f5d74ceeb6bebe8f2c4e913e44e56a5f02
Afek96 <adam.malcherczyk96@gmail.com>
2023-12-13 20:08:37 +0100

    Day 5 Begin

---
+++ AdventOfCode2023/Day05/Fertilizer.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day05
+{
+    internal class Fertilizer : Base
+    {
+        public override string GetFirstResult(string inputText)
+        {
+            string result = "";
+
+            return result;
+        }
+
+        public override string GetSecondResult(string inputText)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2023Tests/Day05/FertilizerTests.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.Day05;
+
+namespace AdventOfCode2023Tests.Day05
+{
+    public class FertilizerTests
+    {
+        Fertilizer _sut;
+
+        string _testInput =
+            @"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+        public FertilizerTests()
+        {
+            _sut = new Fertilizer();
+        }
+
+        [Fact()]
+        public void GetFirstResultTest()
+        {
+            var result = _sut.GetFirstResult(_testInput);
+
+            Assert.Equal("35", result);
+        }
+
+        [Fact()]
+        public void GetSecondResultTest()
+        {
+            var result = _sut.GetSecondResult(_testInput);
+
+            Assert.Equal("??", result);
+        }
+    }
+}
